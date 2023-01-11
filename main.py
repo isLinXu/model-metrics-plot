8038b92efe46f2058e9924f224841106b0baf448
isLinXu <2267379130@qq.com>
2023-01-11 14:41:43 +0800

    1.update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+
 from plots.metrics_plots import plot_metrics
 from utils.dataloader import pd_read_csv
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Quick use model-metrics-plot")
+parser.add_argument('-c', '--csv', default='data/yolo_model_data.csv', help="csv path")
+parser.add_argument('-n', '--name',default='plot_metrics.jpg', help="figure name")
+argparse = parser.parse_args()
+
+
 if __name__ == '__main__':
-    csv_path = '/home/linxu/PycharmProjects/model-metrics-plot/data/yolo_model_data.csv'
+    csv_path = 'data/yolo_model_data.csv'
     fig_name = 'plot_metrics.jpg'
     df = pd_read_csv(csv_path)
     plot_metrics(df, fig_name)
