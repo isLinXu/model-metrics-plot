280e1cd81617f84e6292cfee57e9692d0e0b89f2
isLinXu <2267379130@qq.com>
2023-01-12 10:19:53 +0800

    1.update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from utils.dataloader import pd_read_csv
 import argparse
 
 parser = argparse.ArgumentParser(description="Quick use model-metrics-plot")
-parser.add_argument('-c', '--csv', default='data/yolo_model_data.csv', help="csv path")
+parser.add_argument('-c', '--csv', default='data/yolo_models_data.csv', help="csv path")
 parser.add_argument('-n', '--name',default='plot_metrics.jpg', help="figure name")
 argparse = parser.parse_args()
 
 
 if __name__ == '__main__':
-    csv_path = 'data/yolo_model_data.csv'
+    csv_path = 'data/yolo_models_data.csv'
     fig_name = 'plot_metrics.jpg'
     df = pd_read_csv(csv_path)
     plot_metrics(df, fig_name)
