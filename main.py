00f5e551e728387bd96f8941a5679ce0d9768603
isLinXu <2267379130@qq.com>
2023-01-16 11:46:45 +0800

    update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def parse_opt(known=False):
     parser = argparse.ArgumentParser(description="Quick use model-metrics-plot")
     parser.add_argument('-c', '--csv', default='data/Pytorch_models_data.csv', help="csv path")
     parser.add_argument('-n', '--fig_path', default='output/plot_metrics.jpg', help="figure path")
-    parser.add_argument('-t', '--title_name', default='MS COCO Object Detection', help="title_name")
-    parser.add_argument('-x', '--xlabel_name', default='PyTorch FP16 RTX3080(ms/img)', help="xlabel_name")
-    parser.add_argument('-y', '--ylabel_name', default='COCO Mask AP val', help="ylabel_name")
+    parser.add_argument('-t', '--title_name', default='MS COCO Object Detection', help="title name")
+    parser.add_argument('-g', '--is_grid', default=False, help="title name")
+    parser.add_argument('-x', '--xlabel_name', default='PyTorch FP16 RTX3080(ms/img)', help="xlabel name")
+    parser.add_argument('-y', '--ylabel_name', default='COCO Mask AP val', help="ylabel name")
     parser.add_argument('-f', '--font_size', default=10, help="font_size")
 
     return parser.parse_known_args()[0] if known else parser.parse_args()
-
 
 
 def main(opt):
@@ -24,8 +24,9 @@ def main(opt):
     xlabel_name = opt.xlabel_name
     ylabel_name = opt.ylabel_name
     font_size = opt.font_size
+    is_grid = opt.is_grid
     df = pd_read_csv(csv_path)
-    plot_metrics(df, fig_path, title_name, xlabel_name, ylabel_name, font_size)
+    plot_metrics(df, fig_path, title_name, xlabel_name, ylabel_name, font_size,is_grid)
 
 
 if __name__ == '__main__':
