f0576384ac8df679d397a01fdd4ab92f76639011
isLinXu <2267379130@qq.com>
2023-06-19 23:58:14 +0800

    update __init__.py

--- mmplot/plots/__init__.py
+++ mmplot/plots/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) isLinXu. All Rights Reserved
 from .metrics_plots import *
+from .bar_chart_plot import *
+from .mult_chart_plot import *
 
 __all__ = ["plot_metrics"]
--- plots/__init__.py
+++ plots/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) isLinXu. All Rights Reserved
 from .line_metrics_plots import *
-
+from .bar_chart_plot import *
+from .mult_chart_plot import *
 __all__ = ["plot_metrics"]
