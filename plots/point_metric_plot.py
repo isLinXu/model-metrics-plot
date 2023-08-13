c19758fa9644ad621c9531597b5b0ee762fd5ac4
isLinXu <2267379130@qq.com>
2023-08-13 13:31:09 +0800

    [plots] update point_metric_plot.py

--- plots/point_metric_plot.py
+++ plots/point_metric_plot.py
@@ -25,9 +25,42 @@ def scatter_plot(data_file, x_col, y_col):
     plt.title('{} vs {}'.format(x_col, y_col))
     plt.xlabel(x_col)
     plt.ylabel(y_col)
-
+    plt.savefig('../output/{}_vs_{}.png'.format(x_col, y_col))
     # 显示图表
     plt.show()
 
-# 示例：绘制 'Average' vs 'Java' 的散点图
-scatter_plot('../data/llm_code_eval.csv', 'Average', 'Java')
+def scatter_all_plot(data_file):
+    """
+    绘制散点图
+
+    Args:
+        data_file: str, csv 文件路径
+    """
+    # 读取 csv 文件
+    df = pd.read_csv(data_file)
+
+    # 获取列名
+    cols = df.columns.tolist()
+
+    # 绘制散点图
+    for i in range(1, len(cols)):
+        for j in range(i):
+            x_col = cols[i]
+            y_col = cols[j]
+            x = df[x_col]
+            y = df[y_col]
+            plt.scatter(x, y)
+            plt.xlabel(x_col)
+            plt.ylabel(y_col)
+            plt.title('{} vs {}'.format(x_col, y_col))
+            plt.savefig('../output/{}_vs_{}.png'.format(x_col, y_col))
+            plt.show()
+
+
+
+if __name__ == '__main__':
+    # 示例：绘制 'Average' vs 'Java' 的散点图
+    scatter_plot('../data/llm_code_eval.csv', 'Average', 'Java')
+
+    # 示例：绘制所有列的散点图
+    scatter_all_plot('../data/llm_code_eval.csv')
