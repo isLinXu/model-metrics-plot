239dbd603bc4d63c65a0ca207eb8c24833ac41c5
isLinXu <2267379130@qq.com>
2023-08-17 22:37:43 +0800

    [plots] update bar_chart_plot.jpg

--- plots/bar_chart_plot.py
+++ plots/bar_chart_plot.py
@@ -1,9 +1,37 @@
 import random as random
-
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 
 from utils import pd_read_csv
+
+def bar_chart(data_file, x_col, y_col):
+    """
+    绘制柱状图
+
+    Args:
+        data_file: str, csv 文件路径
+        x_col: str, 横坐标列名
+        y_col: str, 纵坐标列名
+    """
+    # 读取 csv 文件
+    df = pd.read_csv(data_file)
+
+    # 获取数据
+    x = df[x_col]
+    y = df[y_col]
+
+    # 绘制柱状图
+    plt.bar(x, y)
+
+    # 设置图表标题和横纵坐标标签
+    plt.title('{} vs {}'.format(x_col, y_col))
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+
+    # 显示图表
+    plt.show()
+
 
 
 def bar_chart_plot(df, fig_path, value_type='mAP', title_name="MS COCO Object Detection", xlabel_name="COCO mAP(%)",
@@ -54,3 +82,6 @@ if __name__ == '__main__':
     value_type = 'mAP'
     df = pd_read_csv(csv_path)
     bar_chart_plot(df, fig_path, value_type)
+
+    # 示例：绘制 'Metric' 列和 'Java' 列的柱状图
+    bar_chart('../../data/llm_code_eval.csv', 'Metric', 'Java')
