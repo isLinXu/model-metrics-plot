4d9a304c4f0134b5fd3cf56d669a80079a1ebaca
isLinXu <2267379130@qq.com>
2023-08-19 10:49:23 +0800

    [utils] update fonts.py

--- utils/fonts.py
+++ utils/fonts.py
@@ -1,5 +1,9 @@
-
 from matplotlib import rcParams
 
 # 设置全局字体为Times New Roman
 rcParams['font.family'] = 'Times New Roman'
+
+font_new_roman = {'family': 'Times New Roman',
+                  'weight': 'bold',
+                  'size': 18,
+                  }
