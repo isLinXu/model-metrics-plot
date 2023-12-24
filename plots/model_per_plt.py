49996d4af3ce7e2778d2122ca9fb000c0a8a0d8a
isLinXu <2267379130@qq.com>
2023-12-25 01:30:04 +0800

    [plots] add model_per_plt.py

---
+++ plots/model_per_plt.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+
+def extract_data_from_txt(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    operators_data = []
+    for line in lines:
+        if line.startswith('| Conv') or line.startswith('| Relu') or line.startswith('| MaxPool') or line.startswith(
+                '| GlobalAveragePool') or line.startswith('| Flatten') or line.startswith('| Gemm'):
+            operator, count_percentage = line.split(':')
+            operator = operator.strip('| ')
+            count, percentage = count_percentage.split(', ')
+            percentage = float(percentage.strip('percentage='))
+            operators_data.append((operator, percentage))
+    return operators_data
+
+
+def plot_pie_chart(operators_data, file_name):
+    labels = [operator for operator, _ in operators_data]
+    sizes = [percentage for _, percentage in operators_data]
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')
+
+    # save the figure to file
+    plt.title(f"Operators Percentage for {file_name}")  # add title
+    plt.savefig(f'{file_name}_operators_percentage_pie_chart.png')  # save the figure to file
+    plt.show()
+
+
+if __name__ == "__main__":
+    file_path = "/Users/gatilin/PycharmProjects/house-of-model-cards1/model_cards/timm/vgg/vgg11/vgg11.txt"
+    operators_data = extract_data_from_txt(file_path)
+    plot_pie_chart(operators_data, 'vgg11')
